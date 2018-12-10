222d47d2978098c436edbc4e11881c984ced09ac
sudrajadhadi <37056267+sudrajadhadi@users.noreply.github.com>
2018-12-10 17:53:07 +0700

    Add files via upload

---
+++ register/register.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Register</title>
+
+        <!-- Main css -->
+        <link rel="stylesheet" href="register.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <h1>Registration</h1>
+                
+            <form action="" id="join-us">
+                <div class="fields">
+                    <span>
+                        <input placeholder="Name" type="text" />
+                    </span>
+                    <br />
+                    <span>
+                        <input placeholder="Phone" type="text" />
+                    </span>
+                    <br />
+                    <span>
+                        <input placeholder="Email" type="email" />
+                    </span>
+                    <br />
+                    <span>
+                        <input placeholder="Address" type="text" />
+                    </span>
+                    <br />
+                    <span>
+                        <input placeholder="Password" type="password" />
+                    </span>
+                    <br />
+                    <span>
+                        <input placeholder="Re-enter Password" type="password" />
+                    </span>
+                </div>
+                <p> 
+                    if you have already account 
+                    <a href="login.php">Click Here!</a>
+                </p>
+                <div class="submit">
+                    <input class="submit" value="Submit" type="button" />
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
