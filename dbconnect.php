826594b64a7af216a15d404ca33523b33628c113
Iotatfan <zeedmilenium9@gmail.com>
2018-12-18 23:45:47 +0700

    typo

--- dbconnect.php
+++ dbconnect.php
@@ -4,8 +4,44 @@ $user = "root";
 $pass = "";
 $db = "404shop";
 
-$con = mysqli_connect($host, $user, $pass, $db);
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
+$con = mysqli_connect($host,$user,$pass);
+
+if($con->select_db($db)==false) {
+
+    $sql = "CREATE DATABASE $db";
+
+	$con = mysqli_connect($host,$user,$pass,$db);
+
+	if($con->query("CREATE TABLE hewan (
+				id int(11) AUTO_INCREMENT PRIMARY KEY,
+				tipe varchar(25),
+				jenis varchar(25),
+				harga varchar(25),
+				deskripsi text,
+				foto varchar(200) ")===TRUE ) {
+						
+				}
+
+	if($con->query("CREATE TABLE user (
+				id int(11) AUTO_INCREMENT PRIMARY KEY,
+				username varchar(25),
+				phone varchar(13),
+				email varchar(50) NOT NULL,
+				address text,
+				password varchar(30) NOT NULL") === TRUE ) {
+					
+				}
+
+	if($con->query("INSERT INTO hewan (tipe,jenis,harga,deskripsi,foto) VALUES ('Dog','Homete!!!!','Rp 900.000,-','Homete!!! Master!!!','aset/img/homete.jpg')")===TRUE) {
+		
+	}
+	if($con->query("INSERT INTO hewan (tipe,jenis,harga,deskripsi,foto) VALUES ('Hamster','Hamtaro','Rp 350.000,-','Hamataro Yang Imut Suka Makan Biji Bunga Matahari','aset/img/HamtaroN.png')")===TRUE) {
+		
+	}
+	if($con->query("INSERT INTO hewan (tipe,jenis,harga,deskripsi,foto) VALUES ('Dog','Looder Homete??','Rp 850.000,-','Masta-san, lemme paint u','aset/img/art.jpg')")===TRUE) {
+		
+	}
 }
+else $con=mysqli_connect($host, $user, $pass,$db);
+
 ?>
--- storedb.php
+++ storedb.php
@@ -2,44 +2,6 @@
 
 include 'dbconnect.php';
 
-if($con->select_db($db)==false) {
-	$sql = "CREATE DATABASE $db";
-
-	$con = new mysqli($host,$user,$pass,$db);
-
-	if($con->query("CREATE TABLE hewan (
-				id int(11) AUTO_INCREMENT PRIMARY KEY,
-				tipe varchar(25),
-				jenis varchar(25),
-				harga varchar(25),
-				deskripsi text,
-				foto varchar(200) ")===TRUE ) {
-						
-				}
-
-	if($con->query("CREATE TABLE user (
-				id int(11) AUTO_INCREMENT PRIMARY KEY,
-				username varchar(25) NOT NULL,
-				displayname varchar(50),
-				phone varchar(13),
-				email varchar(50),
-				address text,
-				password varchar(30) NOT NULL") === TRUE ) {
-					
-				}
-
-	if($con->query("INSERT INTO hewan (tipe,jenis,harga,deskripsi,foto) VALUES ('Dog','Homete!!!!','Rp 900.000,-','Homete!!! Master!!!','aset/img/homete.jpg')")===TRUE) {
-		
-	}
-	if($con->query("INSERT INTO hewan (tipe,jenis,harga,deskripsi,foto) VALUES ('Hamster','Hamtaro','Rp 350.000,-','Hamataro Yang Imut Suka Makan Biji Bunga Matahari','aset/img/HamtaroN.png')")===TRUE) {
-		
-	}
-	if($con->query("INSERT INTO hewan (tipe,jenis,harga,deskripsi,foto) VALUES ('Dog','Looder Homete??','Rp 850.000,-','Masta-san, lemme paint u','aset/img/art.jpg')")===TRUE) {
-		
-	}
-}
-
-
 $link='';
 		if(isset($_GET['link'])) {
 			$link=$_GET['link'];
