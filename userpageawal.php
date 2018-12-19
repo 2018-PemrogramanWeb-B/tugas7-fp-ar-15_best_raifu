2df4991c4d9f4ef019eb4134c6eef523f6ee9a94
sudrajadhadi <37056267+sudrajadhadi@users.noreply.github.com>
2018-12-19 18:22:16 +0700

    Update userpageawal.php

--- userpageawal.php
+++ userpageawal.php
@@ -1,3 +1,11 @@
+<?php
+  session_start();
+  include 'dbconnect.php';
+  $user = $_SESSION["user"];
+  $data = mysqli_query($con,"select * from user where email='$user'");
+  $edit = mysqli_fetch_array($data);
+?>
+
 <head>
   <title>user_registration</title>
   <meta charset="utf-8">
@@ -36,10 +44,9 @@
         </div>
     </nav>
 
-
     <div class="row">
   		<div class="col-sm-3"><!--left col-->
-              
+
 
       <div class="text-center">
         <img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" class="avatar img-circle img-thumbnail" alt="avatar">
@@ -48,54 +55,54 @@
       </div></hr><br>
 
 
-          
+
         </div><!--/col-3-->
     	<div class="col-sm-9">
           <div class="tab-content">
             <div class="tab-pane active" id="home">
-               
+
                   <form class="form" action="##" method="post" id="registrationForm">
                       <div class="form-group">
-                          
+
                           <div class="col-xs-6">
                               <label for="name"><h4>Name</h4></label>
-                              <input type="text" class="form-control" name="name" id="name" placeholder=" name" title="enter your name .">
+                              <input type="text" class="form-control" name="name" id="name" placeholder="your username" value="<?php echo $edit["username"];?>" title="enter your username.">
                           </div>
                       </div>
-                
+
                       <div class="form-group">
-                          
+
                           <div class="col-xs-6">
                               <label for="phone"><h4>Phone</h4></label>
-                              <input type="text" class="form-control" name="phone" id="phone" placeholder="enter phone" title="enter your phone number if any.">
+                              <input type="text" class="form-control" name="phone" id="phone" placeholder="your phone" value="<?php echo $edit["phone"]; ?>" title="enter your phone number if any.">
                           </div>
                       </div>
                       <div class="form-group">
-                          
+
                           <div class="col-xs-6">
                               <label for="email"><h4>Email</h4></label>
-                              <input type="email" class="form-control" name="email" id="email" placeholder="you@email.com" title="enter your email.">
+                              <input type="email" class="form-control" name="email" id="email" placeholder="your email" value="<?php echo $edit["email"]; ?>" title="enter your email.">
                           </div>
                       </div>
                       <div class="form-group">
-                          
+
                           <div class="col-xs-6">
                               <label for="location"><h4>Location</h4></label>
-                              <input type="location" class="form-control" id="location" placeholder="your location" title="enter your location">
+                              <input type="location" class="form-control" id="location" placeholder="your location" value="<?php echo $edit["alamat"]; ?>" title="enter your location">
                           </div>
                       </div>
                       <div class="form-group">
-                          
+
                           <div class="col-xs-6">
                               <label for="password"><h4>Password</h4></label>
-                              <input type="password" class="form-control" name="password" id="password" placeholder="password" title="enter your password.">
+                              <input type="password" class="form-control" name="password" id="password" placeholder="password" value="<?php echo $edit["katakunci"]; ?>" title="enter your password.">
                           </div>
                       </div>
                       <div class="form-group">
-                          
+
                           <div class="col-xs-6">
                             <label for="password2"><h4>Verify</h4></label>
-                              <input type="password" class="form-control" name="password2" id="password2" placeholder="password2" title="enter your password2.">
+                              <input type="password" class="form-control" name="password2" id="password2" placeholder="password2" value="<?php echo $edit["katakunci"]; ?>" title="enter your password2.">
                           </div>
                       </div>
                       <div class="form-group">
@@ -106,15 +113,15 @@
                             </div>
                       </div>
               	</form>
-              
 
-              
+
+
              </div>
                             </div>
                       </div>
               	</form>
               </div> -->
-               
+
               </div><!--/tab-pane-->
           </div><!--/tab-content-->
 
